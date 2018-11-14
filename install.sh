d5bb0ce5e76dd50d37b5103b704848cafa74d0f0
emma926 <emmawong926@gmail.com>
2018-11-14 17:46:02 +0000

    script to install the packages

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y python-tk
+pip install Cython matplotlib
+pip install 'git+https://github.com/cocodataset/cocoapi#egg=pycocotools&subdirectory=PythonAPI'
+pip install tensor2tensor
